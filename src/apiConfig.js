edb4d395a0295631dcbc3f7fdd09492fe490ca6a
surya <surya@aibond.ai>
2026-05-29 12:57:04 +0530

    Url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://react-loanapp-backend.onrender.com/api';
+export const API_BASE_URL = 'https://react-loanapp-backend.onrender.com/api';
